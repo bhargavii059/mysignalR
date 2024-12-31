1b2186383a3d1fa3c5c5d6fbec2a4b62ec5a5af7
bhargavi.k <bhargavi.k@MKSV-LT-0304>
2024-12-31 18:57:58 +0530

    Add project files.

---
+++ mysignalR/Hubs/Chathub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace mysignalR.Hubs
+{
+    public class Chathub : Hub
+    {
+
+        // This method will be called from the client-side to send a message.
+        public async Task SendMessage(string user, string message)
+        {
+            // Sends the message to all connected clients (broadcasts to all users).
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        // Optionally, you can add client-side connection management methods
+        public override async Task OnConnectedAsync()
+        {
+            // For example, you can broadcast a notification when a user joins
+            await Clients.All.SendAsync("ReceiveMessage", "System", "A new user has connected.");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            // Optionally, broadcast a notification when a user disconnects
+            await Clients.All.SendAsync("ReceiveMessage", "System", "A user has disconnected.");
+            await base.OnDisconnectedAsync(exception);
+        }
+
+
+
+    }
+
+
+}
---
+++ mysignalR/Program.cs
@@ -0,0 +1,46 @@
+using mysignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")  // React's URL
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.MapHub<Chathub>("/chatHub");
+
+
+app.MapControllers();
+
+app.Run();
+//7078 : app
+//3003:react
